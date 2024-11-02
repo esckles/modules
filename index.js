9617197551ca3539308d6ec089b508e87f6c82ee
esckles <davidsamzzywilliams1234@gmail.com>
2024-11-02 21:40:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+// const  dontBlock = (time) => {
+//     console.log("i am running...");
+
+// }
+
+//setimeout
+// setTimeout(() => {
+//   console.log("Task");
+
+//   clearTimeout();
+// }, 2000);
+
+//setinterval
+// setInterval(() => {
+//   console.log("task");
+// }, 2000);
+
+//
+
+// let dateTime = new Date().getTime();
+// console.log(dateTime);
+
+// const readTime = (time) => {
+//   while (new Date(time).getTime() < dateTime + time) {
+//     console.log("first");
+//   }
+// };
+
+// readTime(1000);
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// let data = [1, 5, 7, 10];
+// for (let i = 0; i <= data.length - 1; i++) {
+//   console.log(i);
+// }
+
+// let data2 = { name: "peter", score: 9 };
+
+// for of
+
+// for (let i of data2) {
+//     console.log(i)
+// }
+
+//for in
+// for (let i in data2) {
+//   console.log(i);
+// }
+// for (let i in data2) {
+//   console.log(data2[i]);
+// }
+
+// let reverse = (word) => {
+//   let wrd = word.split("").reverse().join("");
+//   return wrd;
+// };
+// console.log(reverse("Amaka"));
+
+// let wrd = "";
+// for (let i = 0; i < 2; i++) {
+//     console.log(word[i]);
+//     wrd = word [i].concat(wrd);
+//     return {
+
+//     }
+// }
+
+// const main = (cb) => {
+//   return cb();
+// };
+
+// main(() => {
+//   console.log("i am cb ");
+// });
+
+// const prompt = require("prompt-sync")();
+
+// let input = prompt("please enter your name:");
+
+// setTimeout(() => {
+//   console.log(`welcome back ${input}`);
+//   clearTimeout();
+// }, 3000);
+
+// console.log("welcome back ", input);
+
+// to us not node mone node index
+
+//word ======> wo-r**d!!!
+
+// const { TfiRuler } = require("React-icons/tfi");
+
+// let x = ["*", "-", "!"];
+// let xx = "wo-r-**d!!!";
+// let wx = xx.split("");
+
+// let w = "*";
+
+// console.log(
+//   wx
+//     .filter((el) => {
+//       return !x.includes(el);
+//     })
+//     .join("")
+// );
---
+++ logic.ts
@@ -0,0 +1,5 @@
+// const sumUP3 = (n: number): number => {
+//   return 5 (n * (n + 1)) /2
+// };
+
+console.log("hi");
